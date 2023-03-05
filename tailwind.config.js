8ca438feb1b6ef0a9252df7f3414de2648ff4857
Iliyas M <im.smiliyas@gmail.com>
2023-03-05 16:28:00 +0530

    update branding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -14,6 +15,9 @@ module.exports = {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                primary: colors.red,
+            }
         },
     },
 
